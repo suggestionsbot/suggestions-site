5706f9a415bd78ebee8355b926b78c643d826636
acollierr17 <gamemaniac101@gmail.com>
2019-02-17 03:09:18 -0500

    Code split routes

--- website/src/main.js
+++ website/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueYouTubeEmbed from 'vue-youtube-embed';
-import Buefy from 'buefy';
+import { Modal, Tabs, Icon } from 'buefy/dist/components';
 import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
 
-Vue.use(Buefy);
+Vue.use(Modal);
+Vue.use(Tabs);
+Vue.use(Icon);
 Vue.use(VueYouTubeEmbed);
 
 new Vue({
--- website/src/router/index.js
+++ website/src/router/index.js
@@ -3,13 +3,13 @@ import Router from 'vue-router';
 import Meta from 'vue-meta';
 
 // import routes
-// import Home from '../views/Home.vue';
-// import About from '../views/About.vue';
-// import Privacy from '../views/Privacy.vue';
-// import Terms from '../views/Terms';
-// import Contact from '../views/Contact.vue';
-// import Partners from '../views/Partners.vue';
-import TBD from '../views/TBD.vue';
+const Home = () => import('@/views/Home.vue');
+const About = () => import('@/views/About.vue');
+const Privacy = () => import('@/views/Privacy.vue');
+const Terms = () => import('@/views/Terms');
+const Contact = () => import('@/views/Contact.vue');
+const Partners = () => import('@/views/Partners.vue');
+const TBD = () => import('@/views/TBD');
 
 const routes = [
     {
@@ -18,40 +18,42 @@ const routes = [
         component: TBD,
         title: 'Home'
     },
-    // {
-    //     path: '/',
-    //     name: 'home',
-    //     component: Home,
-    //     title: 'Home'
-    // },
-    // {
-    //     path: '/about',
-    //     name: 'about',
-    //     component: About
-    // },
-    // {
-    //     path: '/privacy',
-    //     name: 'privacy',
-    //     component: Privacy
-    // },
-    // {
-    //     path: '/terms',
-    //     name: 'terms',
-    //     component: Terms
-    // },
-    // {
-    //     path: '/contact',
-    //     name: 'contact',
-    //     component: Contact
-    // },
-    // {
-    //     path: '/partners',
-    //     name: 'partners',
-    //     component: Partners
-    // },
     {
-        path: '*',
-        redirect: '/'
+        path: '/home',
+        redirect: '/',
+        name: 'home',
+        component: Home,
+        title: 'Home'
+    },
+    {
+        path: '/about',
+        redirect: '/',
+        name: 'about',
+        component: About
+    },
+    {
+        path: '/privacy',
+        redirect: '/',
+        name: 'privacy',
+        component: Privacy
+    },
+    {
+        path: '/terms',
+        redirect: '/',
+        name: 'terms',
+        component: Terms
+    },
+    {
+        path: '/contact',
+        redirect: '/',
+        name: 'contact',
+        component: Contact
+    },
+    {
+        path: '/partners',
+        redirect: '/',
+        name: 'partners',
+        component: Partners
     }
 ];
 
