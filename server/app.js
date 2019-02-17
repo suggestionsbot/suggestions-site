046bf962444317d7d41f83c5367a93918a95d35f
acollierr17 <gamemaniac101@gmail.com>
2019-02-17 03:11:35 -0500

    Add rest of files... oops

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -8,7 +9,11 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, '/public/')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/public/')));
+
+    app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+}
 
 app.get('/api', (req, res, next) => {
     res.status(404).json({ 
@@ -24,5 +29,8 @@ app.get('/api/v1', (req, res, next) => {
      });
 });
 
-const port = process.env.PORT || 3000;
+let port;
+if (process.env.NODE_ENV === 'production') port = process.env.PORT;
+else port = 3000;
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
