572c4a1610ee15e07599fed4076ced10fea26278
Anthony Collier <hello@acollier.dev>
2021-09-13 21:23:09 -0400

    :truck: Move all files into root directory

--- website/pages/invite.vue
+++
@@ -1,7 +0,0 @@
-<script>
-export default {
-  fetch({ params, redirect }) {
-    redirect(302, 'https://discordapp.com/oauth2/authorize?client_id=474051954998509571&scope=bot&permissions=355392')
-  }
-}
-</script>
