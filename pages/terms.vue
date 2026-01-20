c0a52b0ad5ac7f4d723df59207f903c3a1a4c939
skelmis <skelmis.craft@gmail.com>
2026-01-20 18:55:38 +1300

    updated tos and privacy policy

--- pages/terms.vue
+++ pages/terms.vue
@@ -3,60 +3,38 @@
     <section class="section">
       <div class="container">
         <h1 class="title ">Terms of Service</h1>
-        <h2 class="subtitle">Last updated: January 1, 2026</h2>
+        <h2 class="subtitle">Last updated: January 20, 2026</h2>
 
-        <p>Welcome to Suggestions! These Terms of Service (&quot;Terms&quot;) govern your use of the Suggestions Bot
-          (known on Discord as "Suggestions#2602" with the user ID "474051954998509571") and our websites located at <a
-            href="https://suggestions.gg"
-          >https://suggestions.gg</a>,
-          <a href="https://suggestions.bot">https://suggestions.bot</a> and <a href="https://suggestionsbot.com">https://suggestionsbot.com</a>
-          (collectively, the &quot;Service&quot;), which are operated by Oof Software Limited (&quot;we&quot;,
-          &quot;us&quot;, or &quot;our&quot;). By accessing or using the Service, you agree to be bound by these Terms.
-          If you disagree with any part of the terms, please do not use our Service.</p>
+        <p>Welcome to Suggestions! These Terms of Service ("TOS") govern your use of the Suggestions Bot (known on Discord as "Suggestions#2602" with the user ID "474051954998509571") and our websites located at <a href="https://suggestions.gg">https://suggestions.gg</a>, <a href="https://suggestions.bot">https://suggestions.bot</a> and <a href="https://suggestionsbot.com">https://suggestionsbot.com</a> (collectively, the "Service"), which are operated by Oof Software Limited ("we", "us", or "our"). By accessing or using the Service, you agree to be bound by the TOS. If you disagree with any part of the terms, please do not use our Service.</p>
 
         <h2 class="subtitle is-4">1. Acceptance of Terms</h2>
-        <p>By using the Service, you agree to these Terms. If you are using the Service on behalf of an organization,
-          you are agreeing to these Terms for that organization and promising that you have the authority to bind that
-          organization to these terms.</p>
+        <p>By using the Service, you agree to these Terms. If you are using this application on behalf of an organization, you are agreeing to these Terms for that organization and ensuring that your organization will adhere to the TOS.</p>
 
         <h2 class="subtitle is-4">2. Changes to Terms</h2>
-        <p>We reserve the right to modify or replace these Terms at any time at our sole discretion. </p>
+        <p>Oof Software Limited reserve the right to modify or replace the TOS at any time at our sole discretion. Any Changes to the TOS will be communicated through our Discord Server.</p>
 
         <h2 class="subtitle is-4">3. Use of the Service</h2>
 
         <h3 class="subtitle is-5">3.1 Content Submission</h3>
-        <p>Users can upload content through the Suggestions Bot. However, such content is subject to Discord&#39;s Terms
-          of Service and Community Guidelines. Users are solely responsible for the content they submit.</p>
+        <p>Any content uploaded to Suggestion bot or any of its affiliated services is subject to Discord's Terms of Service and Community Guidelines. Users are solely responsible for the content they submit.</p>
 
         <h3 class="subtitle is-5">3.2 Intellectual Property</h3>
-        <p>The Service and its original content (excluding content provided by users), features, and functionality,
-          including but not limited to the Suggestions Bot name, logo, and other branding elements, are and will remain
-          the exclusive property of Oof Software Limited and its licensors.</p>
+        <p>The Service and its original content (excluding content provided by users), features, and functionality, including but not limited to the Suggestions Bot name, logo, and other branding elements, are and will remain the exclusive property of Oof Software Limited and its licensors.</p>
 
         <h2 class="subtitle is-4">4. User Feedback</h2>
-        <p>Any feedback, comments, ideas, improvements, or suggestions (collectively, &quot;Feedback&quot;) provided by
-          you to us with respect to the Service shall remain the sole and exclusive property of Oof Software Limited. We
-          shall be free to use, copy, modify, publish, or redistribute the Feedback for any purpose and in any way
-          without any credit or any compensation to you.</p>
+        <p>Any feedback, comments, ideas, improvements, or suggestions (collectively, "Feedback") provided by you to us with respect to the Service shall remain the sole and exclusive property of Oof Software Limited. Oof Software Limited shall be free to use, copy, modify, publish, or redistribute Feedback for any purpose and in any way without any credit or compensation.</p>
 
         <h2 class="subtitle is-4">5. Privacy Policy</h2>
-        <p>Your use of the Service is also governed by our Privacy Policy, which is incorporated by reference into these
-          Terms. Please read our Privacy Policy carefully for information relating to our collection, use, and
-          disclosure of your personal information.</p>
+        <p>Use of the Service is governed by the Privacy Policy, which is incorporated into the TOS. Please read our Privacy Policy carefully for information relating to our collection, use, and disclosure of your personal information.</p>
 
         <h2 class="subtitle is-4">6. Limitation of Liability</h2>
-        <p>To the fullest extent permitted by applicable law, Oof Software Limited shall not be liable for any indirect,
-          incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred
-          directly or indirectly, or any loss of data, use, goodwill, or other intangible losses, resulting from (a)
-          your access to or use of or inability to access or use the Service; (b) any conduct or content of any third
-          party on the Service.</p>
+        <p>To the fullest extent permitted by applicable law, Oof Software Limited shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses, resulting from (a) your access to, use of or inability to access or use the Service; (b) any conduct or content of any third party on the Service.</p>
 
         <h2 class="subtitle is-4">7. Governing Law</h2>
         <p>These Terms shall be governed and construed in accordance with the laws of New Zealand.</p>
 
         <h2 class="subtitle is-4">8. Contact Us</h2>
-        <p>If you have any questions about the Terms of Service, please <a :href=contactLink target="_blank">contact
-          us</a>.</p>
+        <p>If you have any questions about the Terms of Service, please contact us.</p>
       </div>
     </section>
   </div>
